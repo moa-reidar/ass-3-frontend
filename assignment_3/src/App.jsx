583e8380c3e444e58a31d064f3847fec043a2b91
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-04-24 23:58:32 +0200

    Started implementing localStorage support for expenses

--- assignment_3/src/App.jsx
+++ assignment_3/src/App.jsx
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import './css/reset.css';
 import './css/variables.css';
 import './css/style.css';
-import ExpenseForm from './components/ExpenseForm'; 
+import ExpenseForm from './components/ExpenseForm';
 
 function App() {
-  const [expenses, setExpenses] = useState([]);
+  const [expenses, setExpenses] = useState(() => {
+    const storedExpenses = localStorage.getItem('expenses');
+    return storedExpenses ? JSON.parse(storedExpenses) : [];
+  });
 
+  // Legger til nye utgifter
   const handleAddExpense = (expenseData) => {
     const newExpense = {
       ...expenseData,
-      id: Math.random().toString(), // gir enn midlertidig unik ID
+      id: Math.random().toString(), // gir en midlertidig unik ID
     };
     setExpenses((prev) => [newExpense, ...prev]);
     console.log('Lagret utgift:', newExpense);
