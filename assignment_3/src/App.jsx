67df26473b2c3abda92d245a703377c3de4b98d3
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-04-24 11:35:09 +0200

    Connected ExpenseForm to App and implemented expense state handling

--- assignment_3/src/App.jsx
+++ assignment_3/src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/reset.css';
 import './css/variables.css';
 import './css/style.css';
+import ExpenseForm from './components/ExpenseForm'; 
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  const handleAddExpense = (expenseData) => {
+    const newExpense = {
+      ...expenseData,
+      id: Math.random().toString(), // gir enn midlertidig unik ID
+    };
+    setExpenses((prev) => [newExpense, ...prev]);
+    console.log('Lagret utgift:', newExpense);
+  };
+
   return (
     <div className="app">
       <h1 className="app__title">Expense Tracker</h1>
-      {/* <ExpenseForm /> */}
+      <ExpenseForm onAddExpense={handleAddExpense} />
     </div>
   );
 }
